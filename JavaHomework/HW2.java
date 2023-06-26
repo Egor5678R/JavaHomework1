0a630cf05195373b2009fb73eca511499f7c87ec
Egor5678R <114798208+Egor5678R@users.noreply.github.com>
2023-06-26 11:41:44 +0300

    Update HW2.java

--- JavaHomework/HW2.java
+++ JavaHomework/HW2.java
@@ -40,7 +40,7 @@ public class HW2 {
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("log.txt"));
 
-            for(int i = 0; i<arr.length - 1; i++) {
+            for(int i = 0; i < arr.length - 1; i++) {
                 for(int j = 0; j < arr.length - i - 1; j++) {
 
                     if(arr[j] > arr[j + 1]) {
